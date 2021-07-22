1ccb2f6053c90f1431d5f3747873f9a32a7e622a
Ayush Singh <82327294+ayush-321@users.noreply.github.com>
2021-07-22 19:22:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,64 @@
+var button = document.getElementsByClassName('btn');
+
+var display = document.getElementById('display');
+
+var operand1 = 0;
+var operand2 = null;
+var operator = null;
+
+for (var i = 0; i < button.length; i++) {
+    button[i].addEventListener('click', function () {
+        var value = this.getAttribute('data-value');
+        var text = display.textContent.trim();
+
+        if (value == '+') {
+            operator = '+';
+            operand1 = parseFloat(text);
+            display.textContent = "";
+        } else if (value == '-') {
+            operator = '-';
+            operand1 = parseFloat(text);
+            display.textContent = "";
+        } else if (value == '*') {
+            operator = '*';
+            operand1 = parseFloat(text);
+            display.textContent = "";
+        } else if (value == '/') {
+            operator = '/';
+            operand1 = parseFloat(text);
+            display.textContent = "";
+        } else if (value == '%') {
+            operator = '%';
+            operand1 = parseFloat(text);
+            operand1 = operand1 / 100;
+            display.textContent = "";
+        } else if (value == 'sign') {
+            operator = 'sign';
+            operand1 = parseFloat(text);
+            operand1 = -1 * operand1;
+            display.textContent = operand1;
+        } else if (value == 'AC') {
+            display.textContent = "";
+        } else if (value == '=') {
+            operand2 = parseFloat(text);
+            var result = eval(operand1 + ' ' + operator + ' ' + operand2);
+            if (result) {
+                display.textContent = result;
+                operand1 = result;
+                operand2 = null;
+                operator = null;
+            }
+
+            if(result / 0){
+                var error = "Error";
+                display.textContent = error;
+            }
+        } else if (value == '.') {
+            if (text.length && !text.includes('.')) {
+                display.textContent = text + '.';
+            }
+        } else {
+            display.innerText += value;
+        }
+    });
+}
